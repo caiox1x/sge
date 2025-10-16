0cca58cf2aaeb10a9340ae1e0980925a1acb79c0
caiox1x <caiodocarmo00@gmail.com>
2025-10-15 23:00:01 -0400

    oitavo commit: começando as melhorias na tela de dashboard

--- app/views.py
+++ app/views.py
@@ -4,8 +4,12 @@ from . import metrics
 
 def home(request):
     product_metrics = metrics.get_product_metrics()
+    sales_metrics = metrics.get_sales_metrics()
+    
 
     context = {
-        'product_metrics': product_metrics
+        'product_metrics': product_metrics,
+        'sales_metrics': sales_metrics,
+        'daily_sales_data': None
     }
     return render(request, 'home.html', context)
