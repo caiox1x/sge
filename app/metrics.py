c02dc2796e8a2583c6f886109a160d058ba85482
caiox1x <caiodocarmo00@gmail.com>
2025-10-21 23:14:57 -0400

    nono commit finalizando graficos de vendas

--- app/metrics.py
+++ app/metrics.py
@@ -1,5 +1,5 @@
-from datetime import timezone
-from django.db.models import Sum
+from django.utils import timezone
+from django.db.models import Sum, F
 from django.utils.formats import number_format
 from products.models import Product
 from outflows.models import Outflow
@@ -25,7 +25,7 @@ def get_sales_metrics():
     total_sales_value = sum(outflow.quantity * outflow.product.selling_price for outflow in Outflow.objects.all())
     total_sales_cost = sum(outflow.quantity * outflow.product.cost_price for outflow in Outflow.objects.all())
     total_sales_profit = total_sales_value - total_sales_cost
-    
+
     return dict(
         total_sales=total_sales,
         total_products_sold=total_products_sold,
@@ -35,3 +35,32 @@ def get_sales_metrics():
 
 def get_daily_sales_data():
     today = timezone.now().date()
+    dates = [str(today - timezone.timedelta(days=i)) for i in range(6, -1, -1)]
+    values = list()
+
+    for date in dates:
+        sales_total = Outflow.objects.filter(
+            created_at__date=date
+        ).aggregate(
+            total_sales=Sum(F('product__selling_price') * F('quantity'))
+        )['total_sales'] or 0
+        values.append(float(sales_total))
+
+    return dict(
+        dates=dates,
+        values=values,
+    )
+
+def get_daily_sales_quantity_data():
+    today = timezone.now().date()
+    dates = [str(today - timezone.timedelta(days=i)) for i in range(6, -1, -1)]
+    quantities = list()
+
+    for date in dates:
+        sales_quantity = Outflow.objects.filter(created_at__date=date).count()
+        quantities.append(sales_quantity)
+
+    return dict(
+    dates=dates,
+    values=quantities,
+    )
